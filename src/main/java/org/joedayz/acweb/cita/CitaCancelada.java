66440f35a4632f2db69eadbef16c99c16f5201d7
Javatar <jesus.mariaca@upeu.edu.pe>
2018-06-20 16:03:34 -0500

    Estados Cita

---
+++ src/main/java/org/joedayz/acweb/cita/CitaCancelada.java
@@ -0,0 +1,9 @@
+package org.joedayz.acweb.cita;
+
+public class CitaCancelada implements EstadoCita {
+
+	public String getEstado() {
+		return "CANCELADA";
+	}
+
+}
---
+++ src/main/java/org/joedayz/acweb/cita/EstadoCita.java
@@ -0,0 +1,5 @@
+package org.joedayz.acweb.cita;
+
+public interface EstadoCita {
+	public String getEstado();
+}
